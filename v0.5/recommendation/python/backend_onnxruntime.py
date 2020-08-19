ad8b5ea3ea7a6cf05dac3fa6fa18b7573cc3ce40
dkorchevgithub <dk@mail.dimaonline.xyz>
2020-08-18 18:02:46 -0700

    updating predict

--- v0.5/recommendation/python/backend_onnxruntime.py
+++ v0.5/recommendation/python/backend_onnxruntime.py
@@ -20,28 +20,40 @@ class BackendOnnxruntime(backend.Backend):
         """Name of the runtime."""
         return "onnxruntime"
 
-    def image_format(self):
-        """image_format. For onnx it is always NCHW."""
-        return "NCHW"
+#    def image_format(self):
+#        """image_format. For onnx it is always NCHW."""
+#        return "NCHW"
 
     def load(self, model_path, inputs=None, outputs=None):
+        
+        inputs = None
+        outputs = None
+        print("onnx load", model_path, inputs, outputs)
         """Load model and find input/outputs from the model file."""
         opt = rt.SessionOptions()
         # enable level 3 optimizations
         # FIXME: enable below once onnxruntime 0.5 is released
         # opt.set_graph_optimization_level(3)
         self.sess = rt.InferenceSession(model_path, opt)
+        
         # get input and output names
-        if not inputs:
+        if inputs is None:
             self.inputs = [meta.name for meta in self.sess.get_inputs()]
         else:
             self.inputs = inputs
-        if not outputs:
+        
+        if outputs is None:
             self.outputs = [meta.name for meta in self.sess.get_outputs()]
         else:
             self.outputs = outputs
+        
+        print("inputs", self.inputs)
+        print("outputs", self.outputs)
+        #self.outputs = ["predict"]
         return self
 
-    def predict(self, feed):
+    def predict(self, batch_dense_X, batch_lS_o, batch_lS_i):
+        print("onnx predict")
         """Run the prediction."""
-        return self.sess.run(self.outputs, feed)
+        return self.sess.run(output_names=self.outputs, input_feed=[{self.inputs[0]:batch_dense_X.numpy()}, {self.inputs[1]:batch_lS_o.numpy()}, {self.inputs[2]:batch_lS_i.numpy()}])
+        
